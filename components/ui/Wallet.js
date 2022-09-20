9df908a486b95e2f77eadde600ab8d6e021c401f
Paras <paras.gaur@skynox.tech>
2022-09-20 16:24:30 +0530

    background fix

--- components/ui/Wallet.js
+++ components/ui/Wallet.js
@@ -20,7 +20,7 @@ export default function Wallet({ walletList, accountAddr}) {
   return (
     <div>
       <div class="grid my-10 items-center justify-center text-center">
-        <div className="max-w-sm w-96 rounded-lg overflow-hidden shadow-lg">
+        <div className="max-w-sm w-96 rounded-lg overflow-hidden shadow-lg bg-white">
           <div className="px-6 py-4 font-bold border-b border-gray-300">
             Add New Wallet
           </div>
@@ -68,14 +68,14 @@ export default function Wallet({ walletList, accountAddr}) {
                               <td className="py-4 px-6">
                                 { element.walletAddress == state.selectedWallet ?
                                       <button
-                                        className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full opacity-50 cursor-not-allowed"
+                                        className="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed"
                                         disabled >
                                         Selected Wallet
                                       </button>
                                   :
                                     <Link href='/admin' className="text-white hover:bg-blue-700">
                                       <button
-                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded-full"
+                                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
                                         onClick={() => setSelectedWalletAddr(element.walletAddress)}>
                                         Use Wallet
                                       </button>
